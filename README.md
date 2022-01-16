f38629e229bf496e7f0249ba9dd3ec62db8e38ea
davistr <davistr1216@gmail.com>
2022-01-16 18:59:36 -0500

    updated README

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# weather-dashboard
+# Weather Dashboard
+
+## Purpose
+* Weather dashboard application that allows you to search the current weather conditions of any city and displays a 5 day forecast.
+
+## Built With:
+* HTML
+* CSS
+* Javascript
+
+## Screenshot
+![](images/site_screenshot.jpg)
+
+## Website
+https://davistr.github.io/weather-dashboard/
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,15 @@ var today = new Date();
 
 var cityFormEl = document.querySelector("#search-form");
 var cityInputEl = document.querySelector("#city");
+var citySearchEl = document.querySelector("#search-history");
 var weatherInputEl = document.querySelector("#weather-container");
 var forecastInputEl = document.querySelector("#forecast-container");
 var counter = 6;
 var key = 0;
 
-
+// use city name for API call
 var getCities = function (cityName) {
     var apiKey = "72f0e6a86d77bd4ccda5c2b150f20525";
-
 
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey).then(function (response) {
         console.log(response);
@@ -31,21 +31,17 @@ var getCities = function (cityName) {
             displayForecast(data);
         });
     });
-
 };
 
 var displayWeather = function (data) {
 
     var fahrenheit = Math.round(((parseFloat(data.main.temp) - 273.15) * 1.8) + 32);
-    // data.name.classList.add = ("city");
-
     var iconCode = data.weather[0].icon;
 
+    document.getElementById('weather-container').innerHTML = "<span style='font-size:32px; font-weight:bolder'>" + data.name + "  " + today.toLocaleDateString("en-US") + "</span>" + " " + "<img src=\"http://openweathermap.org/img/wn/" + iconCode + "@2x.png\">" + "<br />" + "Temp: " + fahrenheit
+        + "&deg" + "F" + "<br />" + "Wind: " + data.wind.speed + " MPH" + "<br />" + "Humidity: " + data.main.humidity + "%" + "<br />";
 
-    document.getElementById('weather-container').innerHTML = data.name + " " + today.toLocaleDateString("en-US") + "  " + "<img src=\"http://openweathermap.org/img/wn/" + iconCode + "@2x.png\">" + "<br />" + "Temp: " + fahrenheit
-        + "&deg" + "F" + "<br />" + "Wind: " + data.wind.speed + " MPH" + "<br />" + "Humidity: " + data.main.humidity + "%" + "<br />" + "UV Index:  " + data.uvi;
-
-    weatherInputEl.setAttribute('style', 'margin-bottom:20px; border: 3px solid darkcyan;');
+    weatherInputEl.setAttribute('style', 'margin-bottom:20px; border: 3px solid darkcyan; padding: 12px;');
 };
 
 var displayForecast = function (data) {
@@ -56,6 +52,7 @@ var displayForecast = function (data) {
 
         var forecastEl = document.createElement('div');
         forecastEl.className = 'forecast';
+        // forecastDate.toLocaleDateString("en-US");
 
         forecastEl.innerHTML = data.list[counter].dt_txt + "<br />" + "<img src=\"http://openweathermap.org/img/wn/" + iconCode + "@2x.png\">" + "<br /v>" + "Temp: " + Math.round(((parseFloat(data.list[counter].main.temp) - 273.15) * 1.8) + 32) +
             "&deg" + "F" + "<br />" + "Wind: " + data.list[counter].wind.speed + " MPH" +
@@ -64,20 +61,26 @@ var displayForecast = function (data) {
         forecastInputEl.appendChild(forecastEl);
         counter += 8;
     }
+
 };
 
-
+// save cities to localstorage
 var saveStorage = function (key, cityName) {
     localStorage.setItem(key, cityName);
-}
+};
 
-
+// load localstorage to page
 var loadStorage = function () {
 
-    // for (var i = 0; i < localStorage.length; i++) {
-    //     localStorage[i].
-    // }
-}
+    for (var i = 0; i < localStorage.length; i++) {
+        var searchEl = document.createElement('button');
+        searchEl.className = 'search-item';
+
+        var city = localStorage.getItem(i);
+        searchEl.innerHTML = city;
+    }
+    citySearchEl.append(searchEl);
+};
 
 
 var formSubmitHandler = function (event) {
@@ -88,14 +91,15 @@ var formSubmitHandler = function (event) {
     if (cityName) {
         getCities(cityName);
         cityInputEl.value = "";
+        saveStorage(key, cityName);
+        loadStorage();
     }
     else {
         alert("Please enter a city name!");
     }
 
-    saveStorage(key, cityName);
     key++;
 };
 
-
+// event listener for 
 cityFormEl.addEventListener("submit", formSubmitHandler);
